d4802bd8c8025caabc50b8051f9e98557d189b29
IT0413 <galaxyyyy.s.0413@gmail.com>
2017-01-23 13:26:38 +0900

    Add:管理対象外になって居たので

---
+++ calculator/RightMenuViewController.swift
@@ -0,0 +1,21 @@
+//
+//  RightMenuViewController.swift
+//  calculator
+//
+//  Created by 伊藤孝史 on 2017/01/22.
+//  Copyright © 2017年 伊藤孝史. All rights reserved.
+//
+
+import Foundation
+import UIKit
+//右から左へスワイプした際に表示されるメニュー用のViewController
+class RightMenuViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //TODO: Write Right Menu Contents
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+}
